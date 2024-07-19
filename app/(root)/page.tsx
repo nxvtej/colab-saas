944dfcc7f1bba66da92ce0a7b84d764d212d5181
nxvtej <nxvtej@outlook.com>
2024-07-19 22:17:16 +0530

    add auth & editor

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { Butcherman } from 'next/font/google'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button name='click me'/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/Header.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+// type HeaderProps = {
+//     children: React.ReactNode
+// }
+
+// done in types file
+const Header = ({children} ) => {
+  return (
+    <div className='header'>
+        <Link href={'/'} className='md:flex-1'>
+        <Image 
+        alt='Logo with name'
+        src="/assets/icons/logo.svg" 
+        width={120}
+        height={32}
+        className='hidden md:block'/>
+        <Image 
+        alt='Logo'
+        src="/assets/icons/logo-icon.svg" 
+        width={32}
+        height={32}
+        className='mr-2 md:hidden'/>
+
+
+
+        </Link>
+
+
+        {children}
+        
+        </div>
+  )
+}
+
+export default Header
